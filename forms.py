5641ea55c0bb1df3fba2f62545e059a91b927ae3
tj <tojaku@gmail.com>
2014-12-01 19:06:03 +0100

    Initial translations completed

--- forms.py
+++ forms.py
@@ -3,7 +3,11 @@ from wtforms import StringField, PasswordField
 from wtforms.validators import Email, DataRequired
 from flask.ext.babel import lazy_gettext as _
 
+# Common messages
+msg_required = _('Field is required')
+msg_email = _('Must be valid e-mail address')
+
 
 class SignInForm(Form):
-    email = StringField(_('E-mail'), validators=[DataRequired(), Email()])
-    password = PasswordField(_('Password'), validators=[DataRequired()])
+    email = StringField(_('E-mail'), validators=[DataRequired(msg_required), Email(msg_email)])
+    password = PasswordField(_('Password'), validators=[DataRequired(msg_required)])
--- lotg.py
+++ lotg.py
@@ -34,7 +34,7 @@ def get_timezone():
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 @app.route('/sign_in', methods=('GET', 'POST'))
