ac3f6701bc0d986d3e9182b7e14adeba90a86df6
tojaku <tojaku@gmail.com>
2014-12-02 14:29:06 +0100

    Sign up process not finished

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import private
 
 DEBUG = True
 SECRET_KEY = 'ov:U7>9*D1/S0&Zf$#7E1@a,t.jF-U+Q'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' % private.MYSQL_USERNAME % ':'\
-                          % private.MYSQL_PASSWORD % '@localhost:3306/lotgpy'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + private.MYSQL_USERNAME + ':'\
+                          + private.MYSQL_PASSWORD + '@localhost:3306/lotgpy'
 BABEL_DEFAULT_LOCALE = 'hr'
 BABEL_DEFAULT_TIMEZONE = 'Europe/Zagreb'
 LANGUAGES = {
